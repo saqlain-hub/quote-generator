33a69a3af8f335f12237cc199c166ce4c83bcf4a
saqlain-hub <saqlainsj12@gmail.com>
2023-10-01 07:43:26 +0500

    Random Quote Generator JavaScript Practice

---
+++ main.js
@@ -0,0 +1,38 @@
+const quotes = [
+  {
+    quote: `"The best way to find yourself is to lose yourself in the service of others."`,
+    person: `Mahatma Gandhi`,
+  },
+  {
+    quote: `"At his best, main is the noblest of all animals; separated from law and justice he is the worst."`,
+    person: `Aristotle`,
+  },
+  {
+    quote: `"If you want to live a happy life, tie it to a goal, not to peopler or things."`,
+    person: `Albert Einstein`,
+  },
+  {
+    quote: `"Your time is limited, so dont waste it living someone else's life."`,
+    person: ` Steve Jobs`,
+  },
+  {
+    quote: `"Tell me and I forget. Teach me and I remember. Involve me and I learn"`,
+    person: `Benjamin Franklin`,
+  },
+  {
+    quote: `"If you look at what you have in life, you'll always have more. If you look at what you don;t have in life, you'll never have enough."`,
+    person: `Oprah Winfrey`,
+  },
+  {
+    quote: `"Our lives begin to end the day we become silent about things that matter."`,
+    person: `Martin Luther King, Jr.`,
+  },
+  {
+    quote: `"Remember that not getting what you want is sometimes a wonderful stroke of luck."`,
+    person: `Dalai Lama`,
+  },
+  {
+    quote: `"The journey of a thousand miles begins with one step."`,
+    person: `Lao Tzu`,
+  },
+];
